c56fd6dc08d9d72df1eed423ec77ed877dfc6d75
Ryan Niklas <rniklas25@gmail.com>
2022-08-09 17:27:40 -0400

    callbacks

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -1,6 +1,8 @@
 _callbacks = {}
 
-def register(0, hook):
-    register_callback(func):
+def register(hook, order=0):
+    def register_callback(func):
+        _callbacks.setdefault(hook, {}).setdefault(order, []).append()
         return func
+
     return register_callback
