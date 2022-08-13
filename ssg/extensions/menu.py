205580b334f0e362f398e513a0ea944e2fcbebf7
Ryan Niklas <rniklas25@gmail.com>
2022-08-13 10:51:08 -0400

    menu

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -9,3 +9,4 @@ def collect_files(source, site_parsers):
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext(path.suffix):
+                files.append(path)
