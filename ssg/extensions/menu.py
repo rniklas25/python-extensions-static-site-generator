84d1e32fb410ce271f68a07995f73bfe50338156
Ryan Niklas <rniklas25@gmail.com>
2022-08-13 10:41:20 -0400

    callbacks

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -3,3 +3,9 @@ from ssg import hooks, parsers
 
 files = []
 
+
+@hooks.register("collect_files")
+def collect_files(source, site_parsers):
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
+    for parser in list(filter(valid, site_parsers)):
+            if parser.valid_file_ext
