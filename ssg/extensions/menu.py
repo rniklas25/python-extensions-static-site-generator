80d1bbb57c5bbd548572304f4d95b2feed20eb90
Ryan Niklas <rniklas25@gmail.com>
2022-08-13 10:49:02 -0400

    callbacks

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -8,4 +8,4 @@ files = []
 def collect_files(source, site_parsers):
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for parser in list(filter(valid, site_parsers)):
-            if parser.valid_file_ext
+            if parser.valid_file_ext(path.suffix):
