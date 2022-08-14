1d5c2d9b3d5dfef4f1d1059909ca213ef0273266
Ryan Niklas <rniklas25@gmail.com>
2022-08-13 20:51:23 -0400

    menu

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -11,3 +11,6 @@ def collect_files(source, site_parsers):
             if parser.valid_file_ext(path.suffix):
                 files.append(path)
 
+@hooks.register("generate_menu")
+def generate_menu(html, ext):
+    template = '<li><a href="{}{}">{}</a></li>'
